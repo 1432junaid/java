925aed368cb7c1a4f0927cc89bd91f3f69d93295
1432junaid <1432junaid@gmail.com>
2020-03-21 12:54:07 +0530

    gukfasha

---
+++ table.java
@@ -0,0 +1,11 @@
+class table{
+	public static void main(String a[]){
+	//	int x = 2;
+		for(int j =1;j<=10;j++){
+			for(int i=1;i<=10; i++){
+				System.out.print("\t"+(j*i) );
+			}
+			System.out.print("\n");
+		}
+	}
+}
